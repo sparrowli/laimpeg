0a68db7d88c0b42c3df584e66366b4e81d3b31ee
sparrow <280669537@qq.com>
2018-05-25 15:32:19 +0800

    Initialize ffmpeg xcode project. :pear: sparrowli

---
+++ laimpeg/laimpeg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  laimpeg
+//
+//  Created by sparrow on 25/5/2018.
+//  Copyright © 2018 sparrow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
